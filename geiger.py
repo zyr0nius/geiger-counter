90cd8dbdd07f59cbc7b130e4eecd683675b327bf
sedzinfo <30319353+sedzinfo@users.noreply.github.com>
2023-09-17 16:42:01 +0300

    Update geiger.py

--- geiger.py
+++ geiger.py
@@ -1,77 +1,69 @@
+
 #!/usr/bin/env python3
 
 from datetime import datetime, timedelta
-import RPi.GPIO as gpio
+from prometheus_client import start_http_server, Counter, Gauge
+import ASUS.GPIO as gpio
 import time
+import os
+import threading
+
+gpio.setwarnings(False)
+os.system("clear")
+
+total_count_metric=Counter("total_count","Total count of events")
+value_per_hour_metric=Gauge("value_per_hour","Value per hour in Sv/h")
+counter_metric=Gauge("counter","Count per minute")
 
 def impulse(channel):
-    """
-    Is called when event is triggered. Increments the counters by 1
-    and adds a timestamp to a list.
-    """
-
     global total_count
     global counter
     global timer
-
     timer.append(datetime.now())
     total_count += 1
     counter += 1
 
 def init():
-    '''
-    Initialises the counters, the list with timestamps and
-    gpio for usage.
-    '''
-
     global total_count
     global counter
     global timer
-
-    gpio_pin = 0
-    total_count = 0
-    counter = 0
-    timer = []
-
-    # use pin layout of rasberry pi and set pin as input
+    global total_count_metric
+    global value_per_hour_metric
+    global counter_metric
+    gpio_pin=7
+    total_count=0
+    counter=0
+    timer=[]
     gpio.setmode(gpio.BOARD)
     gpio.setup(gpio_pin, gpio.IN)
-    # trigger event when signal on pin is falling
-    gpio.add_event_detect(gpio_pin, gpio.FALLING)
-    gpio.add_event_callback(gpio_pin, impulse)
+    gpio.add_event_detect(gpio_pin,gpio.FALLING)
+    gpio.add_event_callback(gpio_pin,impulse)
+    start_http_server(8000)
+    threading.Thread(target=update_prometheus_metrics).start()
+
+def update_prometheus_metrics():
+    while True:
+        value_per_hour=round(counter/151,5)
+        total_count_metric.inc(total_count)
+        counter_metric.set(counter)
+        value_per_hour_metric.set(value_per_hour)
+        time.sleep(60)
 
 def main():
-    """
-    Checks continually if a timestamp is older than 60 s, removes
-    those timestamps from the list and decrements the counter. This
-    ensures that only impulses are counted which are within the last minute.
-    Computes from the number of impulses the value in microsievert per hour.
-    """
-
     global counter
     global timer
-
     init()
-
     try:
         while True:
             for i in timer:
                 if i + timedelta(seconds=60) < datetime.now():
                     timer.pop(0)
                     counter -= 1
-            
-            # The value 151 is not just some random value. You can find
-            # it in the documentation of 'RaditionD-v1.1(CAJOE)'
-            value_per_hour = round(counter / 151, 3)
-
-            # print("Total count: {}".format(total_count), end='\r')
-            print("Current value: {} \u00b5Sv/h".format(value_per_hour), end='\r')
+            value_per_hour=round(counter/151,5)
+            print(f"Current value:{value_per_hour}\u00b5Sv/h Counter:{counter} Total Count:{total_count}",end='\r')
             time.sleep(1)
-
-    # end the program properly when CTRL+C is pressed
     except KeyboardInterrupt:
         gpio.cleanup()
-
 
 if __name__ == "__main__":
     main()
